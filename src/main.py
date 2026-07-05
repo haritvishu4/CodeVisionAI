d780e3a8e22ae90594f89e1b6811d3e570c1840d
haritvishu4 <haritvishu4@gmail.com>
2026-07-05 11:54:12 +0530

    Added import analyzer

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from scanner import scan_project
-from analyzer import count_lines, count_functions
-from analyzer import count_lines
-
+from analyzer import count_lines, count_functions, count_classes
 
 print("🚀 CodeVision AI")
 print("-" * 30)
@@ -12,3 +10,4 @@ scan_project(project_path)
 
 print(f"\nTotal Lines of Code : {count_lines(project_path)}")
 print(f"Total Functions : {count_functions(project_path)}")
+print(f"Total Classes : {count_classes(project_path)}")
